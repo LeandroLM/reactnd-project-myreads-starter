d7fc9af156ededa009f67cd26013eb008839ceab
Leandro Marques <leandrolmarques@gmail.com>
2017-08-30 22:37:25 -0300

    Turn links into Link components

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
 class ListBooks extends React.Component {
@@ -164,7 +165,7 @@ class ListBooks extends React.Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
     )
