85429fdfaa6afba15908d343e142750720fd5305
Leandro Marques <leandrolmarques@gmail.com>
2017-09-03 12:42:43 -0300

    Update SearchBooks component to make Api request with query

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,18 +1,68 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends React.Component {
+  state = {
+    query: '',
+    books: []
+  }
+
+  updateTerm = (query) => {
+    this.setState({ query: query });
+    if (query.trim() === '') {
+      this.setState( { books: [] })
+    } else {
+      BooksAPI.search(query.trim(), 20).then((books) => {
+        if (books.error !== undefined)
+          this.setState({ books: [] })
+        else
+          this.setState({ books })
+      })
+    }
+  }
+
   render() {
+    const { query, books } = this.state;
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateTerm(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {books.map((book) => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    {book.imageLinks && <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")`}}></div>}
+                    <div className="book-shelf-changer">
+                      <select value="currentlyReading">
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  {book.authors && book.authors.map((author, index) => (
+                    <div key={index} className="book-authors">{author}</div>
+                  ))}
+                </div>
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     )
