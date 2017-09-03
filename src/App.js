c76b38d957338a8d3155bd210d02af058be18a86
Leandro Marques <leandrolmarques@gmail.com>
2017-09-02 21:51:54 -0300

    Create function to move books between shelves

--- src/App.js
+++ src/App.js
@@ -2,14 +2,42 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
+  }
+
+  moveBook = (book, prevShelf, shelf) => {
+    BooksAPI.update(book, shelf).then((_books) => {
+      this.setState(prevState => ({
+        [prevShelf]: prevState[prevShelf].filter(b => (b.id !== book.id)),
+        [shelf]: prevState[shelf].concat([book])
+      }))
+    })
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        currentlyReading: books.filter((book) => book.shelf === 'currentlyReading'),
+        wantToRead: books.filter((book) => book.shelf === 'wantToRead'),
+        read: books.filter((book) => book.shelf === 'read')
+      });
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <Route path='/search' component={SearchBooks}/>
-        <Route exact path='/' component={ListBooks}/>
+        <Route exact path='/' render={() => (
+          <ListBooks books={this.state} onMoveBook={this.moveBook} />
+        )}/>
       </div>
     )
   }
